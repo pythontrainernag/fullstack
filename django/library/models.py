e4aef475d5b7e1ed8fd47a0b0322dbc44f11f305
Nageswararao <pythontrainernag@gmail.com>
2018-08-27 17:20:03 +0530

    27/08/2018

--- django/library/models.py
+++ django/library/models.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from docutils.nodes import address
 
 # Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+    url = models.CharField(max_length=50)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+class Book(models.Model):
+    name = models.CharField(max_length = 100)
+    author = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    
